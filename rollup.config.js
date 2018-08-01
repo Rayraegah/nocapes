d4038a14f2c9cdfc9a85d71d812de7c906b1aada
Rayraegah <rayraegah@gmail.com>
2018-08-01 22:53:14 +0900

    Update build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,23 @@ import butternut from "rollup-plugin-butternut";
 const pkg = require("./package.json");
 
 export default {
-	name: pkg.name,
 	input: "src/main.js",
 	output: [
 		{
 			file: pkg.main,
 			format: "cjs",
+			name: pkg.name,
 			sourcemap: pkg.sourcemap
 		},
 		{
 			file: pkg.module,
 			format: "es",
+			name: pkg.name,
 			sourcemap: pkg.sourcemap
 		},
 		{
 			file: pkg.unpkg,
+			name: pkg.name,
 			format: "umd"
 		}
 	],
